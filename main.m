f37a976f70fd62251ae73540c317fb22e7ae4d5c
Jimmy N. <20nguyenjh04@gmail.com>
2024-11-23 17:48:14 -0800

    Main with ronald's poisson distribution

--- main.m
+++ main.m
@@ -1,53 +1,30 @@
-format long
-ruptureData = readmatrix('Rupture Data.lvm','FileType', 'text','Delimiter','\t','Range', [22,2,11221,4]);
-N = length(ruptureData);
-x = 1:N; % number of data points
-T = ((3*60)+53)+0.2; % the time in seconds stated on the spreadsheet ~ 03:53.2
-fs = N/T; % sampling frequency
-voltageData = ruptureData(:,1).*-1;
-bData = ruptureData(:,2).*-1;
-cData= ruptureData(:,3).*-1;
+close all; clear
 
-f = (0:N-1)*(fs/N); % frequency vector
+ruptureData = readtable("Raw Rupture Values.xlsx"); % Uploaded Data
+ruptureData = table2array(ruptureData);
 
-figure
-plot(x,voltageData) % graph of raw voltage data over time points
+time = ruptureData(:,1); % potential feature to add: figuring out the time from the spreadsheet automatically
+voltage = ruptureData(:,2); % potential feature to add: figuring out the voltageX from the spreadsheet automatically
 
-figure
-plot(x,bData)
-
-figure
-plot(x,cData)
-
-Xf = fft(voltageData);
-magnitudes = abs(Xf);
-%{
-figure
-plot(f(1:N),magnitudes(1:N)) % graph including DC component, and not adjusting for spectrum symmetry (Frequency domain graph)
-
-figure
-plot(f(2:N/2+1),magnitudes(2:N/2+1)) % graph excluding DC component, and adjusting for spectrum symmetry (Frequency domain graph)
+% Conversion to Force(pN)
+distance = voltage.*(0.7); % 0.7 nm/volt conversion factor was found through experimental data (we can say was found in botvinick's paper)
+% create a function here to find the conversion factor from distance(nm) to force(pN); would need to follow up with botvinick for the method
+convertFactor = 32.5; % this conversion factor was found through botvinick's signal anaylsis (a good oppurtunity for someone to code)
+force = distance.*(convertFactor);
 
 
-% in this graph it looks like there were a lot more peaks than we think there were, so which ones are the Van der Waals, 1 bond, 2 
-% since the professor already provided it us with force (pN), maybe do the fft of the force data instead?
+posForceData = force(force>-0.5); % need to tinker around with 0.5
+nForceData = (posForceData - min(posForceData)) / (max(posForceData)- min(posForceData)) * max(force); % normalization of force data
+binWidth = 0.2;
+binEdges = 0:binWidth:max(force);
+bin_counts = histcounts(nForceData,binEdges);
+prob_density = bin_counts / (sum(bin_counts) * binWidth);
+bin_centers = binEdges(1:end-1) + binWidth / 2;
 
-forceData = ruptureData(:,3);
+figure;
+bar(bin_centers, prob_density, 'FaceColor', 'k');
+xlabel ('Rupture force(pN)')
+ylabel ('Estimated probability density (1/pN)')
+xlim([0 max(force)]);
+ylim([0 max(prob_density)*1.1]); % need to tinker around with 1.1
 
-f = (0:N-1)*(fs/N); % frequency vector
-
-figure
-plot(x,forceData) % graph of raw voltage data over time points
-
-Xf = fft(forceData);
-magnitudes = abs(Xf);
-
-figure
-plot(f(1:N),magnitudes(1:N)) % graph including DC component, and not adjusting for spectrum symmetry (Frequency domain graph)
-
-figure
-plot(f(2:N/2+1),magnitudes(2:N/2+1)) % graph excluding DC component, and adjusting for spectrum symmetry (Frequency domain graph)
-
-% what does it mean?!?!?
-
-%}
